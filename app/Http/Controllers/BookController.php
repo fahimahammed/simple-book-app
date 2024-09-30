5e83b44f59a8ddc65c2ef1c0b667da2de34f2938
fahimahammed <fahimahammed.cse@gmail.com>
2024-10-01 02:08:43 +0600

    updated

--- app/Http/Controllers/BookController.php
+++ app/Http/Controllers/BookController.php
@@ -28,17 +28,28 @@ class BookController extends Controller
 
     public function store(Request $request)
     {
+        $message = [
+            'stock.gte' => 'The stock must be greater than or equal to 0',
+        ];
         // dd($request->all());
         $validatedData = $request->validate([
             'title' => 'required|string|max:255',
             'author' => 'required|string|max:255',
             'isbn' => 'required|string|size:13',
-            'stock' => 'required|integer|min:1',
+            'stock' => 'required|integer|min:0',
             'price' => 'required|numeric|min:0',
-        ]);
+        ], $message);
 
         // Create the book using the validated data
         $book = Book::create($validatedData);
+
+        // $book = new Book();
+        // $book->title = $request->title;
+        // $book->author = $request->author;
+        // $book->isbn = $request->isbn;
+        // $book->price = $request->price;
+        // $book->stock = $request->stock;
+        // $book->save();
 
         return redirect()->route('books.show', $book->id);
     }
